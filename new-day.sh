a868080d984674f31fd0f14de0e3e59f4c78a69b
Skander <skander.kc@gmail.com>
2022-12-03 01:35:32 +0000

    chore: delete template folder

--- new-day.sh
+++ new-day.sh
@@ -7,4 +7,60 @@ new_day_padded=$(printf "%02d\n" $last_day_increment)
 new_day_dirname="day$new_day_padded"
 
 mkdir "$new_day_dirname"
-cp -a template/ "$new_day_dirname"
+
+cat > "$new_day_dirname/main.go" <<EOF
+package main
+
+import (
+	"fmt"
+
+	"github.com/Leyka/Advent-2022/utils"
+)
+
+func main() {
+	input := utils.ReadFile("input.txt")
+
+	fmt.Println("part1:", part1(input))
+	fmt.Println("part2:", part2(input))
+}
+
+func part1(input string) int {
+	return 0
+}
+
+func part2(input string) int {
+	return 0
+}
+
+EOF
+
+cat > "$new_day_dirname/main_test.go" <<EOF
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+const INPUT = \`
+
+\`
+
+func TestPart1(t *testing.T) {
+	result := part1(INPUT)
+	expected := 0
+
+	assert.Equal(t, expected, result)
+}
+
+func TestPart2(t *testing.T) {
+	result := part2(INPUT)
+	expected := 0
+
+	assert.Equal(t, expected, result)
+}
+
+EOF
+
+touch "$new_day_dirname/input.txt"
