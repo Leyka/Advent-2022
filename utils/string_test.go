cbcc8cfeb382403276cf54717a6fdb2e70b6c6af
Skander <skander.kc@gmail.com>
2022-12-04 12:40:58 +0000

    test: minor change utils

--- utils/string_test.go
+++ utils/string_test.go
@@ -8,30 +8,20 @@ import (
 
 func TestFindUniqueCommonLetters(t *testing.T) {
 	unique := true
-
 	assert.Equal(t, []string{}, FindCommonLetters([]string{"abc"}, unique))
-
 	assert.Equal(t, []string{"c"}, FindCommonLetters([]string{"abc", "decf"}, unique))
-
 	assert.Equal(t, []string{"E", "c"}, FindCommonLetters([]string{"Eabc", "dEcf"}, unique))
-
 	assert.Equal(t, []string{"E"}, FindCommonLetters([]string{"Eabc", "dEEcf", "sEEEEE"}, unique))
-}
 
-func TestFindNotUniqueCommonLetters(t *testing.T) {
-	unique := false
-
+	unique = false
+	assert.Equal(t, []string{}, FindCommonLetters([]string{"abc"}, unique))
 	assert.Equal(t, []string{"c", "c", "c"}, FindCommonLetters([]string{"abccc", "deccfc"}, unique))
-
 	assert.Equal(t, []string{"E", "c"}, FindCommonLetters([]string{"Eabc", "dEcfE"}, unique))
 }
 
 func TestRemoveEmptyStringFromArray(t *testing.T) {
 	assert.Equal(t, []string{}, RemoveEmptyStringFromArray([]string{}))
-
 	assert.Equal(t, []string{}, RemoveEmptyStringFromArray([]string{"", " ", "  ", "   "}))
-
 	assert.Equal(t, []string{"a"}, RemoveEmptyStringFromArray([]string{"a", " ", "  ", "   "}))
-
 	assert.Equal(t, []string{"a", "b"}, RemoveEmptyStringFromArray([]string{"a", " ", "  ", "b", "   "}))
 }
