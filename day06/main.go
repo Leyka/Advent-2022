2efd27fe86c57774d7f55946087cdf47584e9165
Skander <skander.kc@gmail.com>
2022-12-13 16:58:50 +0000

    feat: day06 with xor

--- day06/main.go
+++ day06/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"math"
+	"math/bits"
 	"strings"
+	"time"
 
 	"github.com/Leyka/Advent-2022/helpers/utils"
 )
@@ -11,8 +13,20 @@ import (
 func main() {
 	input := strings.TrimSpace(utils.ReadFile("input.txt"))
 
+	start := time.Now()
 	fmt.Println("part1:", part1(input))
 	fmt.Println("part2:", part2(input))
+	end := time.Since(start)
+	fmt.Println("time:", end) // ~ 746.792µs
+	fmt.Println()
+
+	// Xor solution
+	// https://www.mattkeeter.com/blog/2022-12-10-xor
+	start = time.Now()
+	fmt.Println("xorPart1:", xorPart1(input))
+	fmt.Println("xorPart2:", xorPart2(input))
+	end = time.Since(start)
+	fmt.Println("time:", end) // ~ 9.834µs
 }
 
 func part1(input string) int {
@@ -33,6 +47,35 @@ func solve(input string, buffer int) int {
 
 		if utils.HasUniqueChars(chars) {
 			return i + len(chars)
+		}
+	}
+
+	return 0
+}
+
+func xorPart1(input string) int {
+	return xor(input, 4)
+}
+
+func xorPart2(input string) int {
+	return xor(input, 14)
+}
+
+func xor(input string, size int) int {
+	var set uint32 = 0
+	for i := range input {
+		// Turn on bits as they enter the window
+		set ^= 1 << (input[i] - 'a') // ex. a = 0b1, b = 0b10, c = 0b100, d = 0b1000 etc.
+		// fmt.Printf("%d: %b\n", set, set)
+
+		// Turn off bits as they leave the window
+		if i >= size {
+			set ^= 1 << (input[i-size] - 'a')
+		}
+
+		// Check the current window and see if we're done
+		if bits.OnesCount32(set) == size {
+			return i + 1
 		}
 	}
 
--- day06/main_test.go
+++ day06/main_test.go
@@ -14,10 +14,26 @@ func TestPart1(t *testing.T) {
 	assert.Equal(t, 11, part1("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"))
 }
 
+func TestPart1Xor(t *testing.T) {
+	assert.Equal(t, 7, xorPart1("mjqjpqmgbljsphdztnvjfqwrcgsmlb"))
+	assert.Equal(t, 5, xorPart1("bvwbjplbgvbhsrlpgdmjqwftvncz"))
+	assert.Equal(t, 6, xorPart1("nppdvjthqldpwncqszvftbrmjlhg"))
+	assert.Equal(t, 10, xorPart1("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"))
+	assert.Equal(t, 11, xorPart1("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"))
+}
+
 func TestPart2(t *testing.T) {
 	assert.Equal(t, 19, part2("mjqjpqmgbljsphdztnvjfqwrcgsmlb"))
 	assert.Equal(t, 23, part2("bvwbjplbgvbhsrlpgdmjqwftvncz"))
 	assert.Equal(t, 23, part2("nppdvjthqldpwncqszvftbrmjlhg"))
 	assert.Equal(t, 29, part2("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"))
 	assert.Equal(t, 26, part2("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"))
+}
+
+func TestPart2Xor(t *testing.T) {
+	assert.Equal(t, 19, xorPart2("mjqjpqmgbljsphdztnvjfqwrcgsmlb"))
+	assert.Equal(t, 23, xorPart2("bvwbjplbgvbhsrlpgdmjqwftvncz"))
+	assert.Equal(t, 23, xorPart2("nppdvjthqldpwncqszvftbrmjlhg"))
+	assert.Equal(t, 29, xorPart2("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"))
+	assert.Equal(t, 26, xorPart2("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"))
 }
